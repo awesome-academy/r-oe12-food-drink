b528f2e3c49d33c432fa0b237f4f119ce5add5bd
tunglam96hd <tunglam96hd@gmail.com>
2019-01-21 14:14:24 +0700

    tempalte-admin

---
+++ app/assets/javascripts/admin/admin.js
@@ -0,0 +1,6 @@
+//= require jquery
+//= require bootstrap-sprockets
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
+//= require javascripts/sb-admin-2.js
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,9 +6,11 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "custom", "images")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "custom", "admin", "images")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w( *.png *.jpg *.jpeg *.gif )
+Rails.application.config.assets.precompile << "admin/admin.js"
+Rails.application.config.assets.precompile << "admin/admin.css"
